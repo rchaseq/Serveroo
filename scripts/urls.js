cfbdb67c30f243ac22680f14a242fb4fdd698d51
Rowan Quinn <35435338+rchaseq@users.noreply.github.com>
2022-07-18 16:38:59 -0400

    Update urls.js

--- scripts/urls.js
+++ scripts/urls.js
@@ -1,1 +1,1 @@
-export const getAPIUrl = () => process.env.NEXT_PUBLIC_API_URL || process.env.API_URL || "https://sea-turtle-app-643yw.ondigitalocean.app/graphql";
+export const getAPIUrl = () => process.env.NEXT_PUBLIC_API_URL || process.env.API_URL || "https://serveroo-backend.herokuapp.com/";
