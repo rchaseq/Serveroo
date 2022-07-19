bd874455cbcd8888abcc9697edb548d3fc6a69c7
Rowan Quinn <35435338+rchaseq@users.noreply.github.com>
2022-07-19 10:08:12 -0400

    Update urls.js

--- scripts/urls.js
+++ scripts/urls.js
@@ -1,1 +1,1 @@
-export const getAPIUrl = () => process.env.NEXT_PUBLIC_API_URL || process.env.API_URL || "https://instaplate-backend.herokuapp.com/";
+export const getAPIUrl = () => process.env.NEXT_PUBLIC_API_URL || process.env.API_URL || "https://serveroo-backend.herokuapp.com/";
